45696f7bd3eaf983d0268352857f1925ddf2f47b
MarekKabala1 <marekkabala1@gmail.com>
2025-09-18 05:41:59 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+// import gardenView from './assets/gardenView.webp';
+
+function App() {
+	return (
+		<div>
+			<h1>Windows Trade Web Page</h1>
+		</div>
+	);
+}
+
+export default App;
